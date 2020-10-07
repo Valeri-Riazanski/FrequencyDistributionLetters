f209cb484d67bc1da5e91d2976c10333d09aaed9
Valeri Riazanski <kot-aldo@yandex.ru>
2020-10-07 17:05:05 +0400

    добавил часть задания про поменять местами ключ-значение

--- src/com/val/riazanski/SortModel.java
+++ src/com/val/riazanski/SortModel.java
@@ -1,6 +1,8 @@
 package com.val.riazanski;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -41,7 +43,26 @@ public class SortModel {
         }
         System.out.println("поменяли местами ключ значение ");
         swap.forEach((k,v) -> System.out.println(k + " " + v));
-        System.out.println("почему пар стало меньше?" + '\n' + "наверное, потому что ключ должен быть уникальным ");
+        System.out.println("почему пар стало меньше?" +
+                '\n' + "наверное, потому что ключ должен быть уникальным " +
+                '\n' + "что делать? Попробуем ключом взять другую коллекцию");
+        //поменяли местами ключ значение
+        System.out.println("опять поменяли местами ключ значение" +
+                '\n' + "печать при заполнении МАР");
+        StringBuilder[] listValue = new StringBuilder[1000];
+        for (int i = 0; i < listValue.length; i++) {
+            listValue[i] = new StringBuilder("");
+        }
+        HashMap<StringBuilder,Character> spin = new HashMap<>();
+        for (int i = 0; i < entries.size(); i++) {
+            listValue[i].append(entries.get(i).getValue().toString());
+            spin.put(listValue[i],entries.get(i).getKey());
+            System.out.println(listValue[i].toString() + " " + entries.get(i).getKey());
+        }
+        System.out.println("после заполнения, что видим в MAP");
+        spin.forEach((k,v) -> System.out.println(k.toString() + " " + v));
+        System.out.println("к вопросу про итератор - как обходит forEach при проходе по MAP");
+
     }
     public static void main(String[] args) {
         String str = "цдлуатцджула'lwkf'qlwrklfq'lfrэудкфдцулатйдцула дцул цудлкй.цудлкдлаоэдукпоэукпо34-05-14-85" +
